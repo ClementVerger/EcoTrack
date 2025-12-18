507cae8f2ff20243ad87a2ecac5a27fa93108117
ClementVerger <clement.verger94@gmail.com>
2025-12-18 13:20:05 +0100

    {fix} Déplacer le fichier main.jsx vers le bon emplacement et mettre à jour les chemins d'importation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import { RouterProvider } from 'react-router-dom'
-import router from '../app/router'
+import router from './app/router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
